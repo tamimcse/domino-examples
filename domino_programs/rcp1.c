66477be82ff26b24d3607775b6f523a10ef36c72
tamimcse <tamim@csebuet.org>
2019-02-21 17:37:35 -0500

    recorded bytes received per port

--- domino_programs/rcp1.c
+++ domino_programs/rcp1.c
@@ -7,9 +7,6 @@
 //Link capacity (per port)
 #define C 40*1024*1024*1024
 
-// Total number of bytes received so far.
-int bytes_received = 0;
-
 //last time when RCP rate was calculated
 int last_time = 0;
 
@@ -19,6 +16,8 @@ int avg_rtt [NUM_PORTS] = {200};
 int control_intervals [NUM_PORTS] = {200};
 //RCP feedback throughput for each port
 int feedback_rate [NUM_PORTS] = {200};
+//bytes recieved per port
+int bytes_received [NUM_PORTS] = {0};
 
 struct Packet {
   int size_bytes;
@@ -42,12 +41,12 @@ void func(struct Packet pkt) {
   avg_rtt[pkt.id] = avg_rtt[pkt.id] * 1 + pkt.rtt * 2;
   
   if ((pkt.time - last_time) < control_intervals[pkt.id]) {
-    bytes_received += pkt.size_bytes; 
+    bytes_received[pkt.id] += pkt.size_bytes; 
   }
   else {
     control_intervals[pkt.id] = avg_rtt[pkt.id];
-    bytes_received = 0;
+    bytes_received[pkt.id] = 0;
 //RCP stability constants alpha=1 beta=.5
-//    feedback_rate[pkt.id] = feedback_rate[pkt.id] * (1 + (((C - (bytes_received/control_intervals[pkt.id]))) - ((pkt.queue/2)/control_intervals[pkt.id]))/C);
+//    feedback_rate[pkt.id] = feedback_rate[pkt.id] * (1 + (((C - (bytes_received[pkt.id]/control_intervals[pkt.id]))) - ((pkt.queue/2)/control_intervals[pkt.id]))/C);
   }
 }
