9e03e09bf3f3e7d699a62365874d2ae365c1e176
tamimcse <tamim@csebuet.org>
2019-02-26 23:18:03 -0500

    .

--- domino_programs/rcp4.c
+++ domino_programs/rcp4.c
@@ -5,16 +5,11 @@
 //last time when RCP rate was calculated
 int last_time = 0;
 
-//RTT per ports
 int avg_rtt = 200;
-//RCP Control Intervals for each port
 int control_intervals [NUM_PORTS] = {200};
-//RCP feedback throughput for each port
 int feedback_rate = 200;
-//bytes recieved per port
-int bytes_received [NUM_PORTS] = {0};
-//incoming rate per port
-int incoming_rate [NUM_PORTS] = {0};
+int bytes_received = 0;
+int incoming_rate = 0;
 
 
 struct Packet {
@@ -31,18 +26,18 @@ struct Packet {
 void func(struct Packet pkt) {
   pkt.id = pkt.dport;
   avg_rtt = (avg_rtt * 49 + pkt.rtt)/50;
-  feedback_rate += (B - ((pkt.queue/avg_rtt)/2) - incoming_rate[pkt.id])/C;
+  feedback_rate += (B - ((pkt.queue/avg_rtt)/2) - incoming_rate)/C;
   //Update feedback throughput to the packet
   if (pkt.feedback_thput > feedback_rate) {
     pkt.feedback_thput = feedback_rate;  
   }
   
   if (pkt.tick % 60 == 0) {
-    incoming_rate[pkt.id] = C - bytes_received[pkt.id];
-    bytes_received[pkt.id] = 0;
+    incoming_rate = C - bytes_received;
+    bytes_received = 0;
   }
   else {
-    bytes_received[pkt.id] += pkt.size_bytes; 
+    bytes_received += pkt.size_bytes; 
   }
 
   
