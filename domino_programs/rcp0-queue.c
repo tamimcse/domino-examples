1f4fc304b24be7563931ca37e1cecd5310196f7a
tamimcse <tamim@csebuet.org>
2019-02-27 20:00:43 -0500

    .

---
+++ domino_programs/rcp0-queue.c
@@ -0,0 +1,42 @@
+// rcp.c
+#define C 1000 //Capacity of the line card
+#define T 50 //Control Interval
+#define A 20 //1000/T
+
+//State variables
+int avg_rtt = 200;
+int feedback_rate = 200;
+int bytes_received = 0;
+int incoming_rate = 0;
+
+//Packet headers and meta-data
+struct Packet {
+  int size_bytes;
+  int rtt;
+  int queue;
+  int feedback_thput;
+  int tick;
+};
+
+void func(struct Packet pkt) {
+  //Calculate running average of RTT
+  avg_rtt = (avg_rtt * 49 + pkt.rtt)/50;
+
+  //Write the throughput in packet header
+  if (pkt.feedback_thput > feedback_rate)
+    pkt.feedback_thput = feedback_rate;  
+  
+  //Control interval has expired, so
+  // calculate the feeback throughput
+  // and reset the state variables
+  if (pkt.tick % T == 0) {
+    incoming_rate = C - bytes_received * A;
+    bytes_received = 0;
+    feedback_rate += (C + (((C - \
+      ((pkt.queue/avg_rtt)/2) - \
+      incoming_rate) * T)/avg_rtt))/C;
+  }
+  else {
+    bytes_received += pkt.size_bytes; 
+  }
+}
