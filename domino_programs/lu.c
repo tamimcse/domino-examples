4a6e7a811511228050bdffcf771d4fef3ca04055
tamimcse <tamim@csebuet.org>
2019-03-11 20:03:48 -0400

    .

--- domino_programs/lu.c
+++ domino_programs/lu.c
@@ -1,6 +1,7 @@
 #define NUM_PORTS 64
 #define C 1000 //Link capacity (per port)
 #define B 2000 //2 * C
+#define LEVEL16_SIZE 65536
 
 //last time when RCP rate was calculated
 int last_time = 0;
@@ -16,20 +17,25 @@ int bytes_received [NUM_PORTS] = {0};
 //incoming rate per port
 int incoming_rate [NUM_PORTS] = {0};
 
+int def_nh = 1;
+int N16[LEVEL16_SIZE] = {0}; 
+
 
 struct Packet {
-  int size_bytes;
-  int rtt;
-  int queue;
-  int feedback_thput;
-  int time;
-  int tick;
   int dport;
-  int id; // array index
+  int daddr;
+  int idx16;
 };
 
 void func(struct Packet pkt) {
-  pkt.id = pkt.dport;
+//  pkt.ifindex = def_nh;
+  pkt.idx16 = pkt.daddr >> 16;
+
+  if (N16[pkt.idx16] != 0) {
+    pkt.dport = N16[pkt.idx16]; 
+  }
+
+/*
   avg_rtt[pkt.id] = (avg_rtt[pkt.id] * 49 + pkt.rtt)/50;
   feedback_rate[pkt.id] += (B - ((pkt.queue/avg_rtt[pkt.id])/2) - incoming_rate[pkt.id])/C;
   //Update feedback throughput to the packet
@@ -44,6 +50,6 @@ void func(struct Packet pkt) {
   else {
     bytes_received[pkt.id] += pkt.size_bytes; 
   }
-
+*/
   
 }
